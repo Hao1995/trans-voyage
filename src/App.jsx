f4e988dc6d41f5a366397b1122e4c701012f3f27
Harry Lu <harry.nan.825@gmail.com>
2024-11-20 21:31:21 +0800

    feat/pick-up-random-station

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TransVoyage from './assets/TransVoyage.svg'
 import './App.css'
+import trainStations from './config/trainStations';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const pickRandomStation = () => {
+    const randomIndex = Math.floor(Math.random() * trainStations.length);
+    const station = trainStations[randomIndex];
+    document.getElementById("selected-station").textContent = station;
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={TransVoyage} alt="TransVoyage" style={{ width: '400px', height: 'auto' }} />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={pickRandomStation}>Pick a Random Station</button>
+        <p id="selected-station"></p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
